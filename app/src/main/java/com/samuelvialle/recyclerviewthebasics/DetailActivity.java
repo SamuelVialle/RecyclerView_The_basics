0da8971e42e913ad196d099abdd40930c6d6fbec
Samuel Vialle <samuelvialle@gmail.com>
2023-10-27 16:27:38 +0200

    Add secondActivity design

---
+++ app/src/main/java/com/samuelvialle/recyclerviewthebasics/DetailActivity.java
@@ -0,0 +1,14 @@
+package com.samuelvialle.recyclerviewthebasics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
